f6d3fd6c34b8d0b8ecf103022ae39af32208ac8e
Luka Bacic <luka.bacic278@gmail.com>
2021-08-14 12:41:26 +1000

    Show fields in view-room component

--- src/app/room-booking/view-room/view-room.component.ts
+++ src/app/room-booking/view-room/view-room.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { environment as env } from 'environments/environment';
+import { ActivatedRoute } from '@angular/router';
+import { Room } from '../model/room';
 
 @Component({
   selector: 'app-view-room',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-room.component.css']
 })
 export class ViewRoomComponent implements OnInit {
+  room: Room | null = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  /**
+   * Retrieve the ID from the route and get the room from API
+   */
+  ngOnInit() {
+    // Get room ID
+    this.route.params.subscribe(params => {
+      this.fetchRoom(params.id);
+    });
   }
 
+  /**
+   * Get room from API
+   *
+   * @param {string} id - room id
+   */
+  async fetchRoom(id: string): Promise<void> {
+    const response = await fetch(`${env.roomsApi}/byId/${id}`);
+    const result = await response.json();
+
+    if (result.success) {
+      console.log(result);
+      this.room = result.data;
+    } else {
+      console.error('Error occurred while retrieving room:\n', result);
+    }
+  }
+
+  /**
+   * Checks if the passed variable is a number
+   *
+   * @param {any} num - variable to check if its a number
+   *
+   * @returns boolean
+   */
+  isNumber(num: any): boolean {
+    return !Number.isNaN(num);
+  }
 }
