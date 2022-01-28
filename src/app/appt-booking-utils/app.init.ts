169c798cd87ccfbf5d799b101e481c76fae2e34c
phyo-kyaw <phy0ky6w@gmail.com>
2022-01-28 16:30:24 +1100

    update keycloak url

--- src/app/appt-booking-utils/app.init.ts
+++ src/app/appt-booking-utils/app.init.ts
@@ -1,11 +1,12 @@
 import { KeycloakService } from 'keycloak-angular';
+import { environment } from '../../environments/environment';
 
 export function initializeKeycloak(keycloak: KeycloakService): () => Promise<boolean> {
     return () =>
         keycloak.init({
             config: {
                 //url: 'https://break-booking.online:8443/auth',
-                url: 'http://120.159.30.85/auth',
+                url: '${environment.homeUrl}/auth',
                 realm: 'break-booking',
                 clientId: 'break-booking-frontend-client',
             },
