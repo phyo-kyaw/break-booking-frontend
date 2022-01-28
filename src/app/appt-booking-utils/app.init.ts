c37b3c49f3836d9c9079061c2531bf2f4cd9b6cb
phyo-kyaw <phy0ky6w@gmail.com>
2022-01-28 18:43:06 +1100

    changes for backend routing in ngix and keycloak

--- src/app/appt-booking-utils/app.init.ts
+++ src/app/appt-booking-utils/app.init.ts
@@ -2,11 +2,12 @@ import { KeycloakService } from 'keycloak-angular';
 import { environment } from '../../environments/environment';
 
 export function initializeKeycloak(keycloak: KeycloakService): () => Promise<boolean> {
+  //var URL = `${environment.homeUrl}/auth`;
     return () =>
         keycloak.init({
             config: {
                 //url: 'https://break-booking.online:8443/auth',
-                url: '${environment.homeUrl}/auth',
+                url: 'http://120.159.30.85:8888/auth',
                 realm: 'break-booking',
                 clientId: 'break-booking-frontend-client',
             },
