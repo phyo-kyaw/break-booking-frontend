d29ba148d75344a14cf7361561703d80df0cf0d2
TL <tlhsieh0610@gmail.com>
2022-02-07 15:40:47 +0800

    delete images from S3 when users submit an editing form

--- src/app/service/images/images.service.ts
+++ src/app/service/images/images.service.ts
@@ -11,6 +11,8 @@ interface UploadResponse {
   };
 }
 
+type DeleteResponse = Omit<UploadResponse, 'data'>;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -30,7 +32,9 @@ export class ImagesService {
     for (const file of files) {
       const formData = new FormData();
       formData.append('file', file, file.name);
-      formDataList.push(this._http.post<UploadResponse>(this._url, formData));
+      formDataList.push(
+        this._http.post<UploadResponse>(`${this._url}/uploadImage`, formData)
+      );
     }
 
     return forkJoin(formDataList).pipe(
@@ -40,10 +44,41 @@ export class ImagesService {
           imageSrc: value.data.url
         }))
       ),
-      //one or more requests failed
+
       catchError(err => {
         console.error(
           'An error occurred while connecting to the API for uploading images',
+          err
+        );
+        return of([{ success: false }]);
+      })
+    );
+  }
+
+  /**
+   * Delete images from S3
+   *
+   * @param value - {url: array}
+   * @return Observerable Object
+   */
+  deleteImages(urls: string[]) {
+    let deleteList: Observable<DeleteResponse>[] = [];
+    for (const url of urls) {
+      deleteList.push(
+        this._http.delete<DeleteResponse>(`${this._url}/deleteImage?url=${url}`)
+      );
+    }
+
+    return forkJoin(deleteList).pipe(
+      map(values =>
+        values.map(value => ({
+          success: value.success
+        }))
+      ),
+
+      catchError(err => {
+        console.error(
+          'An error occurred while connecting to the API for deleting images',
           err
         );
         return of([{ success: false }]);
