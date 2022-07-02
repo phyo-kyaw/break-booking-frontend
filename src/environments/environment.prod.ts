3dd3a346965c5dd7f46aac408beb388c88e50119
phyo-kyaw <phy0ky6w@gmail.com>
2022-07-02 20:14:43 +1000

    updated room api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,21 @@
 
 
-const baseApiUrl = 'http://15.206.126.5';
+//const baseApiUrl = 'http://15.206.126.5';
 export const environment = {
  // production: false,
   production: true,
   homeUrl: '${HOME_URL}',
+  //baseApiUrl: '${HOME_URL}',
   bookingPayment: '${HOME_URL}/api/paymentBooking',
   //homeUrl: 'http://localhost:8080',
   // roomsApi: `${baseApiUrl}:8081/api/v1/app/rooms`,
   // dictApi: `${baseApiUrl}:8086/api/v1/dicts`, // dictionaries
   // roomBooking: `${baseApiUrl}:8081/api/v1/app/bookings`, // room booking controller
   // roomPayment: `${baseApiUrl}:8084/api/v1/payment`,
-  roomsApi: `${baseApiUrl}/api/v1/app/rooms`,
-  dictApi: `${baseApiUrl}/api/v1/dicts`, // dictionaries
-  roomBooking: `${baseApiUrl}/api/v1/app/bookings`, // room booking controller
-  roomPayment: `${baseApiUrl}/api/v1/payment`,
+  roomsApi: '${HOME_URL}/api/v1/app/rooms',
+  dictApi: '${HOME_URL}/api/v1/dicts', // dictionaries
+  roomBooking: '${HOME_URL}/api/v1/app/bookings', // room booking controller
+  roomPayment: '${HOME_URL}/api/v1/payment',
 
   images: 'http://booking.nobrainer.link:8087/api/v1/file/uploadImage' // direct link because its not exposed on the gateway
   //backendhomeUrl: 'break-booking.online/api'
